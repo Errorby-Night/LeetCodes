5dd487e63659a6c9921fd29eee69148214d6bb5b
Srijan Dutta <60324903+Errorby-Night@users.noreply.github.com>
2022-07-15 11:05:14 +0530

    Time: 5 ms (33.56%), Space: 5.9 MB (49.55%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.c
@@ -0,0 +1,21 @@
+
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** generate(int numRows, int* returnSize, int** returnColumnSizes){
+    *returnSize = numRows;
+    int** ans = (int**) malloc(numRows * sizeof(int*));
+    *returnColumnSizes = (int*) malloc(numRows * sizeof(int));
+    for (int i = 0; i < numRows; i++) {
+        ans[i] = (int*) malloc(numRows * sizeof(int));
+        (*returnColumnSizes)[i] = i+1;
+        ans[i][i] = ans[i][0] = 1;
+        for (int j = 1; j < i; j++) {
+            ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
+        }
+    }
+    return ans;
+}
