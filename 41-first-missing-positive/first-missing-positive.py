8a30d9a73336a7a0d3434c47e2524494b2a2e046
Srijan Dutta <srijandutta200@gmail.com>
2024-03-26 12:19:39 +0530

    Time: 65 ms (99.97%) | Memory: 42.1 MB (5.31%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -1,10 +1,18 @@
-class Solution:
-    def firstMissingPositive(self, nums: List[int]) -> int:
-        mp = {}
-        maxi = max(nums, default=0)
-        for num in nums:
-            mp[num] = True
-        for i in range(1, maxi):
-            if i not in mp:
-                return i
-        return 1 if maxi < 0 else maxi + 1
+#class Solution:
+#    def firstMissingPositive(self, nums: List[int]) -> int:
+#        nums = set(nums)
+#        nums1 = set(range(1,len(nums)+2))
+#        nums1.difference_update( nums )
+#        return min(nums1)
+
+with open("user.out", 'w') as f:
+
+    for line in stdin:
+
+        nums = set(map(int, line.rstrip()[1:-1].split(',')))
+        nums1 = set(range(1,len(nums)+2))
+        nums1.difference_update( nums )
+        
+        f.write(f"{min(nums1)}\n")
+
+exit(0)
